fefe9b4bd6b738da3b579b4aad943465b82a70e4
Martin <koo.martin@gmail.com>
2019-02-10 17:55:48 -0500

    allow cors (for microservice) and add error handling

--- phone.js
+++ phone.js
@@ -1,17 +1,26 @@
 import Koa from 'koa';
 import bodyParser from 'koa-bodyparser';
 import { parsePhoneNumberFromString } from 'libphonenumber-js';
+import cors from '@koa/cors';
 
 const app = new Koa();
 app.use(bodyParser());
+app.use(cors({
+  origin: '*'
+}));
 
 app.use(async ctx => {
-  const { phone, country} = ctx.request.body;
-  const phoneNumber = parsePhoneNumberFromString(phone, country);
-  ctx.body = phoneNumber.isValid();
-  ctx.res.end();
-  // Print || log result after response
-  console.log(`${phone}, ${country} was ${phoneNumber.isValid()}`)
+  try {
+    const { phone, country} = ctx.request.body;
+    const phoneNumber = parsePhoneNumberFromString(phone, country);
+    ctx.body = phoneNumber ? phoneNumber.isValid() : false;
+    ctx.res.end();
+    console.log(`${phone}, ${country} was ${ctx.body}`)
+  } catch(e) {
+    ctx.body = false;
+    ctx.res.end();
+    console.log(`ERROR: Attempt to validate ${JSON.stringify(ctx.request.body)} threw:\n${e}`);
+  }
 });
 
 app.listen(3123);
