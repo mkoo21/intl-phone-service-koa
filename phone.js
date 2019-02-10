8437a3d935a7032416b0da8535c292c8add76cab
Martin <koo.martin@gmail.com>
2019-02-09 21:45:53 -0500

    log after response

--- phone.js
+++ phone.js
@@ -8,8 +8,10 @@ app.use(bodyParser());
 app.use(async ctx => {
   const { phone, country} = ctx.request.body;
   const phoneNumber = parsePhoneNumberFromString(phone, country);
-  console.log(`${phone}, ${country} is ${phoneNumber.isValid()}`)
   ctx.body = phoneNumber.isValid();
+  ctx.res.end();
+  // Print || log result after response
+  console.log(`${phone}, ${country} was ${phoneNumber.isValid()}`)
 });
 
 app.listen(3123);
